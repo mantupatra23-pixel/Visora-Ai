3ab7bd8c39953ddff531386e6a99ee85b95fc9a7
Tumhara Name <tumhara-email@example.com>
2025-11-27 11:04:51 +0530

    fix: final Android V2 MainActivity update

--- android/app/src/main/kotlin/com/visora/ai/MainActivity.kt
+++ android/app/src/main/kotlin/com/visora/ai/MainActivity.kt
@@ -1,6 +1,6 @@
-package com.visora.ai;
+package com.visora.ai
 
-import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.android.FlutterActivity
 
-public class MainActivity extends FlutterActivity {
+class MainActivity : FlutterActivity() {
 }
